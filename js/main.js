883783345e4dec6d15fde30bdf7604c6e59a4628
Tushar Srivastava <tushar@hirexp.com>
2020-02-28 12:52:34 +0530

    project urls, contact layout, tech stack css

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.materialboxed').materialbox();
 });
+
+function closeSidenav() {
+  $('.sidenav').sidenav('close');
+}
+
+$(document).ready(function(){
+  $('.tooltipped').tooltip();
+});
