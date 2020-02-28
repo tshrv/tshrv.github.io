dc7c8d577eb3e33b828d9ca31bc439667dccd79d
Tushar Srivastava <tushar@hirexp.com>
2020-02-28 19:00:51 +0530

    applied indentation and removed empty lines

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.tooltipped').tooltip();
   $('.materialboxed').materialbox();
-
 });
 
 function closeSidenav() {
-  var smallScreenMediaQuery = window.matchMedia("(max-width : 992px)");
+  let smallScreenMediaQuery = window.matchMedia("(max-width : 992px)");
   if (smallScreenMediaQuery.matches){
     $('.sidenav').sidenav('close');
   }
