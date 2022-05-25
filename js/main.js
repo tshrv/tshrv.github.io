e60951c00d27386ba7a64341b47c586e6a4ca433
Tushar <tusharsrivastava162@gmail.com>
2022-05-25 23:48:42 +0530

    v3 init

--- js/main.js
+++
@@ -1,5 +0,0 @@
-// explicitly initialize tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
