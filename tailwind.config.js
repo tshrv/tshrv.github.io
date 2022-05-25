760b529f3f4c0b661fc187182b79f7704997fdeb
Tushar Srivastava <49210008+tshrv@users.noreply.github.com>
2022-05-25 20:43:50 +0000

    basic structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui'],
+        'serif': ['ui-serif', 'Georgia'],
+        'mono': ['"JetBrains Mono"', 'monospace'],
+        'display': ['Oswald'],
+        'body': ['"Open Sans"'],
+      }
+    },
+    
+  },
+  plugins: [],
+}
